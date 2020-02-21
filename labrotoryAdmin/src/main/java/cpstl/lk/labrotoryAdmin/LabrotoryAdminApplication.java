367331315745116601e07171659a02ce8111433b
samira001 <samiragj@gmail.com>
2020-02-21 10:58:28 +0530

    initial setup

---
+++ labrotoryAdmin/src/main/java/cpstl/lk/labrotoryAdmin/LabrotoryAdminApplication.java
@@ -0,0 +1,13 @@
+package cpstl.lk.labrotoryAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabrotoryAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabrotoryAdminApplication.class, args);
+	}
+
+}
